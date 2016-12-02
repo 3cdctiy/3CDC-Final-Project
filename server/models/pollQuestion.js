8261b28ea920f019726c1a4e6cb11f5823e3a62c
Tom Gobich <fallofmars@gmail.com>
2016-12-02 15:47:11 -0500

    working on new schemas for polling

---
+++ server/models/pollQuestion.js
@@ -0,0 +1,27 @@
+// The Poll Question model
+
+const mongoose      = require('mongoose');
+const Schema        = mongoose.Schema;
+const bcrypt 				= require('bcryptjs');
+
+
+
+// userSchema structure
+var pollQuestionSchema = new Schema({
+	pollQuestion: {
+		type: String,
+		unique: true,
+		required: true,
+	},
+	pollQuestionSortOrder: {
+		type: Int,
+		unique: true,
+		required: true,
+	},
+	_pollOptions: [{type: Schema.Types.ObjectId, ref: 'PollOptions'}],
+	_pollUser: [{type: Schema.Types.ObjectId, ref: 'User'}]
+});
+
+
+
+module.exports = mongoose.model('PollQuestion', userSchema);
