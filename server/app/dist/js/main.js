eb956e5df99952f686595e7710c6ed83fb6dd42d
[John Patterson] <pattersonjc1@gmail.com>
2016-12-05 16:06:39 -0500

    fixing

--- server/app/dist/js/main.js
+++ server/app/dist/js/main.js
@@ -54,7 +54,7 @@
 			templateUrl: '../partials/landingpage.html',
 			controller: 'CLogin',
 			controllerAs: 'controller'
-		}).state('landing', {
+		}).state('landing2', {
 			url: '',
 			templateUrl: '../partials/landingpage.html',
 			controller: 'CLogin',
--- server/app/js/main.js
+++ server/app/js/main.js
@@ -54,7 +54,7 @@
 			controller: 'CLogin',
 			controllerAs: 'controller'
 		})
-		.state('landing', {
+		.state('landing2', {
 			url: '',
 			templateUrl: '../partials/landingpage.html',
 			controller: 'CLogin',
