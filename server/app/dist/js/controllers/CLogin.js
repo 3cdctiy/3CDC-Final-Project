2ee784e01d3d23baba6319334826e42334361c3a
Tom Gobich <fallofmars@gmail.com>
2016-12-08 13:04:13 -0500

    updated getUpdates checkbox handling

--- server/app/dist/js/controllers/CLogin.js
+++ server/app/dist/js/controllers/CLogin.js
@@ -2,117 +2,140 @@
 
 (function () {
 
-		'use strict';
+  'use strict';
 
-		angular.module('app').controller('CLogin', function ($auth, $location, toastr, FFormUtilities, FApi) {
+  angular.module('app').controller('CLogin', function ($auth, $location, toastr, FFormUtilities, FApi) {
 
-				var vm = this;
-				var userID = void 0;
+    var vm = this;
 
-				vm.showLogin = false;
+    vm.showLogin = false;
 
-				// Initialize tabs
-				$('ul.tabs').tabs();
+    vm.user = {
+      getUpdates: true
+    };
 
-				// Initialize modals
-				$('.modal').modal();
+    // Initialize tabs
+    $('ul.tabs').tabs();
 
-				var checkOpenModal = function checkOpenModal() {
-						if ($auth.isAuthenticated()) {
-								var promise = FApi.getUserUpdateStatus();
-								promise.then(function (response) {
-										var user = response.data;
-										userID = response.data._id;
+    // Initialize modals
+    $('.modal').modal();
 
-										if (!user.hasOwnProperty('isGettingUpdates')) {
-												// Open getUpdates modal
-												$('#getUpdates').modal('open');
-										}
-								});
-								promise.catch(function (error) {
-										toastr.error(response.data.message);
-								});
-						}
-				};
+    var socialSetUserUpdate = function socialSetUserUpdate() {
+      if ($auth.isAuthenticated()) {
+        var promise = FApi.getUserUpdateStatus();
+        promise.then(function (response) {
+          console.log(response.data);
+          var user = response.data;
+          var userID = response.data._id;
 
-				// ------------------------------------------------------------
-				// Name: login
-				// Client side login form handling
-				// ------------------------------------------------------------
-				vm.isLoggedIn = function (isValid, loginForm) {
-						try {
-								if ($auth.isAuthenticated()) {
-										return true;
-								} else {
-										return false;
-								}
-						} catch (error) {
-								toastr.error(error.message, error.name);
-						}
-				};
+          if (!user.hasOwnProperty('isGettingUpdates')) {
+            // Open getUpdates modal
+            submitGetUpdates(userID, vm.user.getUpdates);
+          }
+        });
+        promise.catch(function (error) {
+          toastr.error(response.data.message);
+        });
+      }
+    };
 
-				// ------------------------------------------------------------
-				// Name: login
-				// Client side login form handling
-				// ------------------------------------------------------------
-				vm.login = function (isValid, loginForm) {
-						try {
-								if (!isValid) {
-										throw new Error('Invalid form fields');
-								}
-								$auth.login(vm.user).then(function () {
-										toastr.success('You have successfully signed in!');
-										$location.path('/');
-								}).catch(function (error) {
-										toastr.error(error.data.message, error.status);
-								});
-						} catch (error) {
-								toastr.error(error.message, error.name);
-						}
-				};
+    // ------------------------------------------------------------
+    // Name: submitGetUpdates
+    // Submits a user's answer on getting TIY updates
+    // ------------------------------------------------------------
+    var submitGetUpdates = function submitGetUpdates(userID, answer) {
 
-				// ------------------------------------------------------------
-				// Name: authenticate
-				// Authentication handling
-				// ------------------------------------------------------------
-				vm.authenticate = function (provider) {
-						$auth.authenticate(provider).then(function () {
-								checkOpenModal();
-								toastr.success('You have successfully signed in with ' + provider + '!');
-								$location.path('/');
-						}).catch(function (error) {
-								if (error.message) {
-										// Satellizer promise reject error.
-										toastr.error(error.message);
-								} else if (error.data) {
-										// HTTP response error from server
-										toastr.error(error.data.message, error.status);
-								} else {
-										toastr.error(error);
-								}
-						});
-				};
+      var data = {
+        userID: userID,
+        getUpdates: answer
+      };
 
-				// ------------------------------------------------------------
-				// Name: submitGetUpdates
-				// Submits a user's answer on getting TIY updates
-				// ------------------------------------------------------------
-				vm.submitGetUpdates = function (answer) {
+      var promise = FApi.setGetUpdates(data);
+      promise.then(function (response) {
+        console.log('setting successfully saved: ' + answer);
+      });
+      promise.catch(function (error) {
+        toastr.error(response.data.message);
+      });
+    };
 
-						var data = {
-								userID: userID,
-								getUpdates: answer
-						};
+    // ------------------------------------------------------------
+    // Name: signup
+    // Client side signup form handling
+    // ------------------------------------------------------------
+    vm.signup = function (isValid) {
+      try {
+        if (!isValid) {
+          throw new Error('Please ensure all form fields are valid');
+        }
+        $auth.signup(vm.user).then(function (response) {
+          $auth.setToken(response);
+          $location.path('/');
+          toastr.info('You have successfully created a new account and have been signed-in');
+        }).catch(function (response) {
+          toastr.error(response.data.message);
+        });
+      } catch (error) {
+        toastr.error(error.message, error.name);
+      }
+    };
 
-						var promise = FApi.setGetUpdates(data);
-						promise.then(function (response) {
-								console.log('setting successfully saved: ' + answer);
-						});
-						promise.catch(function (error) {
-								toastr.error(response.data.message);
-						});
-				};
+    // ------------------------------------------------------------
+    // Name: login
+    // Client side login form handling
+    // ------------------------------------------------------------
+    vm.isLoggedIn = function (isValid, loginForm) {
+      try {
+        if ($auth.isAuthenticated()) {
+          return true;
+        } else {
+          return false;
+        }
+      } catch (error) {
+        toastr.error(error.message, error.name);
+      }
+    };
 
-				checkOpenModal();
-		});
+    // ------------------------------------------------------------
+    // Name: login
+    // Client side login form handling
+    // ------------------------------------------------------------
+    vm.login = function (isValid, loginForm) {
+      try {
+        if (!isValid) {
+          throw new Error('Invalid form fields');
+        }
+        $auth.login(vm.user).then(function () {
+          toastr.success('You have successfully signed in!');
+          $location.path('/');
+        }).catch(function (error) {
+          toastr.error(error.data.message, error.status);
+        });
+      } catch (error) {
+        toastr.error(error.message, error.name);
+      }
+    };
+
+    // ------------------------------------------------------------
+    // Name: authenticate
+    // Authentication handling
+    // ------------------------------------------------------------
+    vm.authenticate = function (provider) {
+      $auth.authenticate(provider).then(function () {
+        socialSetUserUpdate();
+        toastr.success('You have successfully signed in with ' + provider + '!');
+        $location.path('/');
+      }).catch(function (error) {
+        if (error.message) {
+          // Satellizer promise reject error.
+          toastr.error(error.message);
+        } else if (error.data) {
+          // HTTP response error from server
+          toastr.error(error.data.message, error.status);
+        } else {
+          toastr.error(error);
+        }
+      });
+    };
+  });
 })();
